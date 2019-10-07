54b09df958da74895da3c1f2fe1f54c18f87aad3
micmine <micmine4@gmail.com>
2019-10-07 13:06:16 +0200

    init

---
+++ update-repositorie.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo Updating the configuration
+rm -rf .config/*
+mkdir -p .config/i3
+mkdir -p .config/polybar
+mkdir -p .config/termite
+mkdir -p Wallpapers
+
+#i3-gaps
+cp ~/.config/i3/config .config/i3/config
+
+# polybar
+cp ~/.config/polybar/launch.sh .config/polybar/launch.sh
+cp ~/.config/polybar/config.ini .config/polybar/config.ini
+
+#termite
+cp ~/.config/termite/config .config/termite/config
+
+#Wallpapers
+cp ~/Wallpapers/* Wallpapers/
+
+#compton
+cp ~/.config/compton.conf .config/
+
+
+echo Finish
