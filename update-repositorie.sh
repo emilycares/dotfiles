de9b3ba9ffc9d8502feed9463c1901ac696496d3
micmine <micmine4@gmail.com>
2019-11-01 23:28:09 +0100

    switch to rofi

--- update-repositorie.sh
+++ update-repositorie.sh
@@ -5,6 +5,7 @@ rm -rf .config/*
 mkdir -p .config/i3
 mkdir -p .config/polybar
 mkdir -p .config/termite
+mkdir -p .config/rofi
 mkdir -p Wallpapers
 
 # i3-gaps
@@ -13,6 +14,9 @@ cp ~/.config/i3/config .config/i3/config
 # polybar
 cp ~/.config/polybar/launch.sh .config/polybar/launch.sh
 cp ~/.config/polybar/config.ini .config/polybar/config.ini
+
+# rofi
+cp ~/.config/rofi/config.rasi .config/rofi/config.rasi
 
 # termite
 cp ~/.config/termite/config .config/termite/config
